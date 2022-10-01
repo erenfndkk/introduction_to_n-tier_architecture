4a6c4cd85ba3cada45cb6cf65a9811417000aae2
Eren Fındık <90691429+erenfndkk@users.noreply.github.com>
2022-10-01 10:10:17 +0300

    Add files via upload

---
+++ DataAccessLayer/Concreate/Context.cs
@@ -0,0 +1,27 @@
+﻿using EntityLayer.Concreate;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concreate
+{
+    public class Context : IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=LAPTOP-OH7D98U9\\SQLEXPRESS;database=DbNewOopCore1;" +
+                "integrated security=true;");
+        }
+         
+        public DbSet<Product> Products { get; set; } // s taksısı alan sql deki tablo adı,
+                                                     // küçüktür büyüktür sembölü içindeki c# daki hali
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Job> Jobs { get; set; }
+    }
+}
---
+++ DataAccessLayer/EntityFramework/EfCustomerDal.cs
@@ -0,0 +1,24 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concreate;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concreate;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EfCustomerDal : GenericRepository<Customer>, ICustomerDal
+    {
+        public List<Customer> GetCustomerListWithJob()
+        {
+            using (var c = new Context())
+            {
+                return c.Customers.Include(x => x.Job).ToList();
+            }
+        }
+    }
+}
